53c388c883464fea3350bdc6fa239d383fce5fdd
gokhanwell <gkhnyrdmcc@gmail.com>
2024-10-06 14:44:56 +0300

    should define your key file

--- provider.tf
+++ provider.tf
@@ -11,11 +11,3 @@ provider "azurerm" {
   }
   subscription_id = var.subscription_id
 }
-
-resource "azurerm_resource_group" "resource_group" {
-  name     = var.rg_name
-  location = var.rg_location
-  tags = {
-    Name = "${var.rg_name}"
-  }
-}
